028459fd051c8081c01f6848212d0bfa647c27c4
Brian <deltoro.brian@gmail.com>
2017-10-24 16:54:10 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,24 @@
+chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+const thing = ['dude'];
+
+function addElementToBeginningOfArray(array,element){
+   var newarray = new Array(element, array);
+   newarray.unshift(element);
+   return newarray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element);
+}
+
+
+//const array = [1];
+//var check1 = addElementToBeginningOfArray(array,'foo');
+//var check2 = destructivelyAddElementToBeginningOfArray(array,'foo');
+//console.log(check1);
+//console.log(check2);
+
+
+addElementToBeginningOfArray(thing, 'yo');
+console.log(thing);
