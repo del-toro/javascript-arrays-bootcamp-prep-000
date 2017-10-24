a87e8cb75430b33fe7071cf0b0d323007dc2a2a7
Brian <deltoro.brian@gmail.com>
2017-10-24 17:22:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,23 +2,16 @@ chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 const thing = ['dude'];
 console.log(thing);
+
+
 function addElementToBeginningOfArray(array,element){
-   var newarray = array;
-   newarray.unshift(element);
+   const newarray = [element,...array];
    return newarray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   return array.unshift(element);
 }
-
-
-//const array = [1];
-//var check1 = addElementToBeginningOfArray(array,'foo');
-//var check2 = destructivelyAddElementToBeginningOfArray(array,'foo');
-//console.log(check1);
-//console.log(check2);
-
 
 addElementToBeginningOfArray(thing, 'yo');
 console.log(thing);
