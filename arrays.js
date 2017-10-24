a93dd409de319b8ddfc931553a18d9b45fa3915c
Brian <deltoro.brian@gmail.com>
2017-10-24 17:41:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,3 +37,9 @@ function destructivelyAddElementToEndOfArray(array, element){
 function accessElementInArray(array,index){
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  var newarray = array;
+  newarray.shift();
+  return newarray;
+}
