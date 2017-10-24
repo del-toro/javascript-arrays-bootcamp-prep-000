cf1dfa6bceb3fd1fabf91f26700bec97ffe03850
Brian <deltoro.brian@gmail.com>
2017-10-24 17:46:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,5 +40,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  return array.slice(1);
+  const newarray = array.slice(1);
+  return newarray;
 }
